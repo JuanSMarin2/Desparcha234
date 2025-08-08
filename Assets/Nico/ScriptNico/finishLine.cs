8a3924df101c1a4ff3593741c9e6b423b041e0bd
nijesa <nisaza570@gmail.com>
2025-08-08 13:35:43 -0500

    Meta terminada
    Cuando un jugador llega a la meta sale un mensaje en la consola

--- Assets/Nico/ScriptNico/finishLine.cs
+++ Assets/Nico/ScriptNico/finishLine.cs
@@ -2,21 +2,23 @@ using UnityEngine;
 
 public class finishLine : MonoBehaviour
 {
+    
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (CompareTag("Player_1"))
+        Debug.Log("Objeto toco");
+        if (collision.CompareTag("Player_1"))
         {
             Debug.Log("Llegó el jugador 1");
         }
-        else if (CompareTag("Player_2"))
+        else if (collision.CompareTag("Player_2"))
         {
             Debug.Log("Llegó el jugador 2");
         }
-        else if (CompareTag("Player_3"))
+        else if (collision.CompareTag("Player_3"))
         {
             Debug.Log("Llegó el jugador 3");
         }
-        else if (CompareTag("Player_4"))
+        else if (collision.CompareTag("Player_4"))
         {
             Debug.Log("Llegó el jugador 4");
         }
