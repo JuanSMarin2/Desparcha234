c92a5909697336f68a67865b1b9cf225cc3c98bd
nijesa <nisaza570@gmail.com>
2025-08-14 09:33:39 -0500

    Terminada Logica Zancos
    Hay una probabilidad del 5% para que se caiga
    Ya integrado con los ganadores

--- Assets/Nico/ScriptNico/ZancoMove.cs
+++ Assets/Nico/ScriptNico/ZancoMove.cs
@@ -1,17 +1,49 @@
 using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 
 public class ZancoMove : MonoBehaviour
 {
     Transform posicion;
+    bool puedeMover = true;
+    bool llegoMeta = false; // Nueva variable de estado
+
     void Start()
     {
         posicion = gameObject.GetComponent<Transform>();
     }
+
+    // Llama este método cuando llegue a la meta
+    public void LlegarMeta()
+    {
+        llegoMeta = true;
+    }
+
     public void MoveZanco()
     {
+        if (llegoMeta) return; // Si llegó a la meta, no se mueve
+        if (!puedeMover) return;
+
         Vector3 nuevaPos = transform.position;
         nuevaPos.x -= 0.2f;
         transform.position = nuevaPos;
+
+        caida();
+    }
+
+    void caida()
+    {
+        int num = Random.Range(0, 100);
+        if (num >= 0 && num < 5)
+        {
+            Debug.Log("Caida");
+            StartCoroutine(BloquearMovimientoPorTiempo(2f));
+        }
+    }
+
+    IEnumerator BloquearMovimientoPorTiempo(float segundos)
+    {
+        puedeMover = false;
+        yield return new WaitForSeconds(segundos);
+        puedeMover = true;
     }
 }
--- Assets/Nico/ScriptNico/finishLine.cs
+++ Assets/Nico/ScriptNico/finishLine.cs
@@ -2,24 +2,36 @@ using UnityEngine;
 
 public class finishLine : MonoBehaviour
 {
-    
+    public ZancoMove z1;
+    public ZancoMove z2;
+    public ZancoMove z3;
+    public ZancoMove z4;
+    public GameRoundManager gameManager;
     void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Objeto toco");
         if (collision.CompareTag("Player_1"))
         {
+            gameManager.PlayerWin(0);
+            z1.LlegarMeta();
             Debug.Log("Llegó el jugador 1");
         }
         else if (collision.CompareTag("Player_2"))
         {
+            gameManager.PlayerWin(1);
+            z2.LlegarMeta();
             Debug.Log("Llegó el jugador 2");
         }
         else if (collision.CompareTag("Player_3"))
         {
+            gameManager.PlayerWin(2);
+            z3.LlegarMeta();
             Debug.Log("Llegó el jugador 3");
         }
         else if (collision.CompareTag("Player_4"))
         {
+            gameManager.PlayerWin(3);
+            z4.LlegarMeta();
             Debug.Log("Llegó el jugador 4");
         }
     }
