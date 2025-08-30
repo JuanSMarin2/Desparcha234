c77bbfa569cfad2e6ca20aef96afcac6f58f61df
LS1653 <estebanpuerta2006@gmail.com>
2025-08-30 11:23:25 -0500

    Corrección script

--- Assets/Scripts/esteban/BarraDeFuerza.cs
+++ Assets/Scripts/esteban/BarraDeFuerza.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BarraFuerza : MonoBehaviour
+public class BarraDeFuerza : MonoBehaviour
 {
     [Header("Config Barra de Fuerza")]
     public Image barraPoder; // Asigna aquí la imagen 'BarraFuerzaPoder'
