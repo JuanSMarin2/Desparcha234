d936642de584a274457875c0060e9356682522b2
JuanSMarin2 <160736940+JuanSMarin2@users.noreply.github.com>
2025-09-12 22:28:22 -0500

    Insertar nombre

--- Assets/Scripts/GameDescriptionTextManager.cs
+++ Assets/Scripts/GameDescriptionTextManager.cs
@@ -6,6 +6,11 @@ public class GameDescriptionTextManager : MonoBehaviour
     [SerializeField] private GameObject textObject;
 
 
+    private void Start()
+    {
+        textObject.SetActive(true);
+    }
+
     public void Continue()
     {
         textObject.SetActive(false);
