d6be1dca59c8651200963edc0df3684d38d0f95c
Jacob <jacobo1304@gmail.com>
2025-10-08 12:21:51 -0500

    Serializamos volumen y cambiamos a .55 en main memu

--- Assets/Jacobo Rodríguez/Code/Scripts/SonidosUI.cs
+++ Assets/Jacobo Rodríguez/Code/Scripts/SonidosUI.cs
@@ -4,12 +4,13 @@ public class SonidosUI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     SoundManager sm;
+    [SerializeField] private float volumen = 0.55f;
     void Start()
     {
         sm = SoundManager.instance;
     }
     public void ClickBoton()
     {
-        if (sm != null) sm.PlaySfx("ui:select", 0.75f);
+        if (sm != null) sm.PlaySfx("ui:select", volumen);
     }
 }
