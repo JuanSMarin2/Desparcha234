c052875593cd7ba6ee10a6ac4ba29a04146149d6
nijesa <nisaza570@gmail.com>
2025-10-24 10:00:21 -0500

    Tiempo de espera de 3s
    Al ganar en zancos espera 3 segundos y cambia la escena

--- Assets/Nico/ScriptNico/finishLine.cs
+++ Assets/Nico/ScriptNico/finishLine.cs
@@ -8,33 +8,44 @@ public class finishLine : MonoBehaviour
     public ZancoMove z4;
     public GameRoundManager gameManager;
     [SerializeField] AudioManagerSacos ams;
+    private System.Collections.IEnumerator HandlePlayerWin(int playerIndex)
+    {
+        Debug.Log("entra a la corrutina");
+        yield return new WaitForSeconds(3f);
+        gameManager.PlayerWin(playerIndex);
+        Debug.Log("Se registra en el manager");
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Objeto toco");
         if (collision.CompareTag("Player_1"))
         {
-            gameManager.PlayerWin(0);
+            StartCoroutine(HandlePlayerWin(0));
+            
             z1.LlegarMeta();
             ams.PlaySFX(ams.Ganar);
             Debug.Log("Llegó el jugador 1");
         }
         else if (collision.CompareTag("Player_2"))
         {
-            gameManager.PlayerWin(1);
+            StartCoroutine(HandlePlayerWin(1));
+            
             z2.LlegarMeta();
             ams.PlaySFX(ams.Ganar);
             Debug.Log("Llegó el jugador 2");
         }
         else if (collision.CompareTag("Player_3"))
         {
-            gameManager.PlayerWin(2);
+            StartCoroutine(HandlePlayerWin(2));
+            
             z3.LlegarMeta();
             ams.PlaySFX(ams.Ganar);
             Debug.Log("Llegó el jugador 3");
         }
         else if (collision.CompareTag("Player_4"))
         {
-            gameManager.PlayerWin(3);
+            StartCoroutine(HandlePlayerWin(3));
+            
             z4.LlegarMeta();
             ams.PlaySFX(ams.Ganar);
             Debug.Log("Llegó el jugador 4");
